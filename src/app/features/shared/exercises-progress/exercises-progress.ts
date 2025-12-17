e0e0b157da8c1b8b202417c3c41a2302bb4dd6b3
Renan R Ramos <renanrramossi@gmail.com>
2025-12-17 19:56:48 -0300

    Improve accordion

---
+++ src/app/features/shared/exercises-progress/exercises-progress.ts
@@ -0,0 +1,46 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { CombinedExercise } from '@domain/combined-exercises.model';
+import { Exercise } from '@domain/exercise.model';
+
+@Component({
+  selector: 'app-exercises-progress',
+  imports: [CommonModule],
+  templateUrl: './exercises-progress.html',
+  styleUrl: './exercises-progress.css',
+})
+export class ExercisesProgress implements OnInit {
+
+  @Input() exercises: Exercise[] = [];
+  @Input() combinedExercises: CombinedExercise[] = [];
+  totalExercises: number = 0;
+  totalCompleted = 0;
+  percentCompleted = 0;
+
+  ngOnInit(): void {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.calculateExercisesPercentual();
+  }
+
+  calculateTotalExercises(): number {
+    let totalExerciseCount = this.exercises.length;
+    
+    // Contar exercícios dentro de conjuntos combinados
+    this.combinedExercises.forEach(combinedSet => totalExerciseCount += combinedSet.exercises.length);
+    return totalExerciseCount;
+  }
+
+  calculateExercisesPercentual(): void {
+      this.totalExercises = this.calculateTotalExercises();
+
+      const completedIndividualExercises = this.exercises.filter(exercise => exercise.completed).length;
+
+      let completedCombinedExercises = 0;
+      this.combinedExercises.forEach(combinedSet =>
+          completedCombinedExercises += combinedSet.exercises.filter(exercise => exercise.completed).length);
+
+      this.totalCompleted = completedIndividualExercises + completedCombinedExercises;
+      this.percentCompleted = this.totalExercises > 0 ? (this.totalCompleted / this.totalExercises) * 100 : 0;
+  }
+}
